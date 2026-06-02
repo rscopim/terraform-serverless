b13468ef7227e558eafe3cd7b2d9bdc34e40c41d
rscopim <ricardo.simines@gmail.com>
2026-06-01 23:47:00 -0300

    Corrige endpoint de leads do PROD

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -50,7 +50,7 @@ module "s3_static_site" {
   project_name = var.project_name
   environment  = var.environment
   bucket_name  = "materiais-e-trilhas-de-estudos"
-  api_endpoint = module.api_gateway.api_endpoint
+  api_endpoint = module.api_gateway.leads_endpoint
 
   index_file_path             = "${path.root}/../../static_site/index.html"
   style_file_path             = "${path.root}/../../static_site/style.css"
