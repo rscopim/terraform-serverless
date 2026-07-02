5c662a4e3de7c6b5648eec11e1a5f7ddee23f0b1
rscopim <ricardo.simines@gmail.com>
2026-07-02 15:46:32 -0300

    feat: add visitor counter API Gateway routes

--- modules/api_gateway/outputs.tf
+++ modules/api_gateway/outputs.tf
@@ -13,3 +13,7 @@ output "api_id" {
 output "execution_arn" {
   value = aws_apigatewayv2_api.this.execution_arn
 }
+
+output "counter_endpoint" {
+  value = "${aws_apigatewayv2_api.this.api_endpoint}/counter"
+}
