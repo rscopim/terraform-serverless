50c772223744a5ef2a88787f588fa9ec7100ef5f
rscopim <ricardo.simines@gmail.com>
2026-07-03 14:53:51 -0300

    feat: add FinOps costs Lambda

---
+++ modules/costs/main.tf
@@ -0,0 +1,102 @@
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_file = var.lambda_source_file
+  output_path = var.lambda_output_path
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "${var.project_name}-${var.environment}-costs-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  tags = {
+    Project     = var.project_name
+    Environment = var.environment
+    ManagedBy   = "Terraform"
+    Feature     = "FinOpsDashboard"
+  }
+}
+
+resource "aws_iam_policy" "lambda_policy" {
+  name = "${var.project_name}-${var.environment}-costs-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ce:GetCostAndUsage"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_policy.arn
+}
+
+resource "aws_lambda_function" "this" {
+  function_name = "${var.project_name}-${var.environment}-costs"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "app.lambda_handler"
+  runtime       = "python3.12"
+
+  filename         = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+
+  timeout     = 10
+  memory_size = 128
+
+  environment {
+    variables = {
+      ALLOWED_ORIGINS = var.allowed_origins
+    }
+  }
+
+  tags = {
+    Project     = var.project_name
+    Environment = var.environment
+    ManagedBy   = "Terraform"
+    Feature     = "FinOpsDashboard"
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.lambda_policy_attachment
+  ]
+}
+
+resource "aws_cloudwatch_log_group" "this" {
+  name              = "/aws/lambda/${aws_lambda_function.this.function_name}"
+  retention_in_days = 7
+
+  tags = {
+    Project     = var.project_name
+    Environment = var.environment
+    ManagedBy   = "Terraform"
+    Feature     = "FinOpsDashboard"
+  }
+}
