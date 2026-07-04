a033bdd8ec03ce66b80120d02f923a4d882b5c80
rscopim <ricardo.simines@gmail.com>
2026-07-04 10:12:59 -0300

    feat: add costs API Gateway route

--- modules/api_gateway/variables.tf
+++ modules/api_gateway/variables.tf
@@ -21,3 +21,11 @@ variable "visitor_counter_lambda_invoke_arn" {
 variable "visitor_counter_lambda_function_name" {
   type = string
 }
+
+variable "costs_lambda_invoke_arn" {
+  type = string
+}
+
+variable "costs_lambda_function_name" {
+  type = string
+}
